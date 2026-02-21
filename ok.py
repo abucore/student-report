d698b681423e17e4465b12e626e5a6a3956f5a0a
abubakr <abucore06@gmail.com>
2026-02-21 17:13:53 +0530

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,54 @@
+student={}
+while True:
+    print("student record system:")
+    print("1.add student")
+    print("2.view student list")
+    print("3.search student")
+    print("4.update marks")
+    print("5.delete student")
+    print("6.exit")
+    a=int(input("choose any option:"))
+    if a==1:
+        name=input("enter the name of the student to add:")
+        maths=int(input("enter mark in maths:"))
+        physics=int(input("enter mark in physics:"))
+        if name in student:
+            print(name,"already exist")
+        else:
+            student[name]={
+                "maths":maths,
+                "physics":physics
+            }
+        print("student added succesfully")
+    elif a==2:
+         for name,marks in student.items():
+            print("name:",name)
+            print("maths:",marks["maths"])
+            print("physics:",marks["physics"])
+    elif a==3:
+        name=input("enter the name to be searched:")
+        if name in student:
+            for subject,score in student[name].items():
+                print(subject,":",score)
+        else:
+            print("student not found")
+    elif a==4:
+        name=input("enter the name:")
+        if name in student:
+            for subject,score in student[name].items():
+                student[name][subject]=int(input(f"enter the mark in {subject}:"))
+            print("updated successfully")
+        else:
+            print("no student found")
+    elif a==5:
+        name=input("enter the name to delete:")
+        if name in student:
+            student.pop(name)
+            print("deleted successfully")
+        else:
+            print("name not found")
+    elif a==6:
+        print("program exiting")
+        break
+    else:
+        print("invalid option chosen")
